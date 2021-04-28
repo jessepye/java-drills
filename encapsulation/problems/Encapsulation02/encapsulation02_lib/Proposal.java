f4264740e1b799c84f563fcf560decdc55cc89b8
Jesse Pye <jessepye@gmail.com>
2021-04-28 15:31:26 -0500

    Encapsulation02 complete

--- encapsulation/problems/Encapsulation02/encapsulation02_lib/Proposal.java
+++ encapsulation/problems/Encapsulation02/encapsulation02_lib/Proposal.java
@@ -1,5 +1,14 @@
 package encapsulation02_lib;
 
 public class Proposal {
-  public String name = "";
+  private String name = "";
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
 }
